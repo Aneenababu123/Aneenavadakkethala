e1c0d2c81361466752309ddb0b1dd35bf5dd7ebe
Aneenababu123 <126000626+Aneenababu123@users.noreply.github.com>
2023-03-05 14:59:21 +0000

    Add files via upload
    added pie chart code

--- Assignment1.py
+++ Assignment1.py
@@ -54,13 +54,36 @@ def barchart(bardata):
     plt.show()
     return # A return statement is used to end the execution of the function call 
 
+'''
+A  user defined function is created to depict the piechart ,also inserted the required data set to plot the chart.
+Which is used for the furthur analysis of  provided data. 
+'''
+
+
+def piechart(piedata):
+    data_3 = piedata # New variable is created to store the dataset.
+
+    print(data_3) # prints the specified dataset for plotting pie chart
+    plt.pie(data_3["2017"], labels=data_3["COUNTRY"], autopct="%1.1f%%")
+    # plot title
+    plt.title('MOBILE SUBSCRIPTION RATE PER 100 PERSON IN THE YEAR OF 2017')
+    plt.savefig("Pieplot.png")
+ 
+    # function to show the plot
+    plt.show()
+    return # A return statement is used to end the execution of the function call 
+
 
 if __name__ == "__main__":
     #To read the data in excel format for linechart
     data_1 = pd.read_excel("data_1.xlsx")  
     #To read the data in excel format for barchart
     data_2 = pd.read_excel("death.xlsx")
+    #To read the data in excel format for piechart
+    data_3 = pd.read_excel("Mobile_sub.xlsx")
     #Here is the function for calling lineplot
     linechart(data_1)
    #Here is the function for calling barchart
     barchart(data_2)
+    #Here is the function for calling piechart
+    piechart(data_3)
