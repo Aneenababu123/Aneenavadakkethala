a61ded7d8b5a7895d8d1f5e34d0c4efc8747520c
Aneenababu123 <126000626+Aneenababu123@users.noreply.github.com>
2023-03-05 14:55:21 +0000

    Add files via upload
    added bar plot code

--- Assignment1.py
+++ Assignment1.py
@@ -33,10 +33,34 @@ def linechart(linedata):
     plt.show()
     return # A return statement is used to end the execution of the function call 
 
+'''
+A user defined function is created to depicts the bar plot ,also inserted the required data set to plot the chart.
+Which is used for the furthur analysis of  provided data. 
+'''
+
+def barchart(bardata):
+    data_2 = bardata # New variable is created to store the dataset.
+    print(data_2) # prints the specified dataset for plotting bar chart
+
+# Set labels for x and y axis
+    plt.figure()
+    plt.bar(data_2["COUNTRY"], data_2["FEMALE"], label="FEMALE")
+    plt.bar(data_2["COUNTRY"], data_2["MALE"], label="MALE")
+    plt.title("CAUSE OF DEATH BY INJURY IN THE YEAR 2019") # Shows the title of chart
+    plt.xlabel("Country") # for x axis
+    plt.ylabel("Death Rate") # for y axis
+    plt.savefig("barplot.png") # Saving the figure with png file extension
+    plt.legend()
+    plt.show()
+    return # A return statement is used to end the execution of the function call 
+
 
 if __name__ == "__main__":
     #To read the data in excel format for linechart
-    data_1 = pd.read_excel("data_1.xlsx")   
+    data_1 = pd.read_excel("data_1.xlsx")  
+    #To read the data in excel format for barchart
+    data_2 = pd.read_excel("death.xlsx")
     #Here is the function for calling lineplot
     linechart(data_1)
-   
+   #Here is the function for calling barchart
+    barchart(data_2)
